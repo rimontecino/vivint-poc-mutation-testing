d402775783d8398d9bb630846dfc8b1cf575f895
Ricardo Montecino <ricardo.montecino@nrg.com>
2025-08-25 10:10:18 -0400

    test: add additional tests to achieve full stryker score

--- MutationTesting.Tests/PriceCalculatorTests.cs
+++ MutationTesting.Tests/PriceCalculatorTests.cs
@@ -29,6 +29,40 @@ public class PriceCalculatorTests
     {
         var calculator = new PriceCalculator();
 
-        Assert.Throws<ArgumentException>(() => calculator.CalculatePrice(-10, 10));
+        // changed price from -10 to 0
+        Assert.Throws<ArgumentException>(() => calculator.CalculatePrice(0, 10));
+    }
+
+    [Fact] // Added test for 0 and 100 discount
+    public void NoExceptionForZeroAnd100Discount()
+    {
+        var calculator = new PriceCalculator();
+
+        var exceptionWhen0 = Record.Exception(() => calculator.CalculatePrice(100, 0));
+        var exceptionWhen100 = Record.Exception(() => calculator.CalculatePrice(100, 100));
+
+        Assert.Null(exceptionWhen0);
+        Assert.Null(exceptionWhen100);
+    }
+
+    [Fact]
+    public void InvalidDiscountPercent_ShouldThrowExceptionWithCorrectMessage()
+    {
+        var calculator = new PriceCalculator();
+
+        var ex1 = Assert.Throws<ArgumentException>(() => calculator.CalculatePrice(100, -1));
+        Assert.Equal("Discount percent must be between 0 and 100.", ex1.Message);
+
+        var ex2 = Assert.Throws<ArgumentException>(() => calculator.CalculatePrice(100, 101));
+        Assert.Equal("Discount percent must be between 0 and 100.", ex2.Message);
+    }
+
+    [Fact]
+    public void InvalidPrice_ShouldThrowExceptionWithCorrectMessage()
+    {
+        var calculator = new PriceCalculator();
+
+        var ex = Assert.Throws<ArgumentException>(() => calculator.CalculatePrice(0, 10));
+        Assert.Equal("Price must be greater than zero.", ex.Message);
     }
 }
